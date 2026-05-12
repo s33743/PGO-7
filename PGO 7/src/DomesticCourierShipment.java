8de183aa1bf8630bd47761769b7f7f300745b7fd
s33743 <s33743@pjwstk.edu.pl>
2026-05-12 22:52:59 +0200

    DomesticCourierShipment

---
+++ PGO 7/src/DomesticCourierShipment.java
@@ -0,0 +1,51 @@
+ public class DomesticCourierShipment extends ShipmentOrder {
+
+        private double packageWeightKg;
+        private boolean weekendDelivery;
+
+        public DomesticCourierShipment(String orderNumber,
+                                       String customerName,
+                                       double distanceKm,
+                                       double baseFee,
+                                       boolean insured,
+                                       double packageWeightKg,
+                                       boolean weekendDelivery) {
+
+            super(customerName, orderNumber, distanceKm, baseFee, insured, 0);
+
+            this.packageWeightKg = packageWeightKg;
+            this.weekendDelivery = weekendDelivery;
+        }
+
+        @Override
+        protected double calculateBasePrice() {
+            return getBaseFee() + getDistanceKm() * 1.20;
+        }
+
+        @Override
+        protected double calculateAdditionalFee() {
+
+            double fee = packageWeightKg * 4.00;
+
+            if (weekendDelivery) {
+                fee += 25;
+            }
+
+            return fee;
+        }
+
+        @Override
+        protected double applyBusinessDiscount(double price) {
+
+            if (getDistanceKm() >= 300) {
+                return price * 0.95;
+            }
+
+            return price;
+        }
+
+        @Override
+        public String getShipmentType() {
+            return "Domestic courier";
+        }
+    }
